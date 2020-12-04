c48d492be483d29b64cfb09861917b5d0ea52a46
jeongyeon5566 <lotuus@naver.com>
2020-12-04 12:24:00 +0900

    리뷰 insert 화면 완성

--- maison/src/main/java/com/kh/maison/review/controller/ReviewController.java
+++ maison/src/main/java/com/kh/maison/review/controller/ReviewController.java
@@ -3,9 +3,11 @@ package com.kh.maison.review.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.maison.review.model.service.ReviewService;
+import com.kh.maison.review.model.vo.Review;
 import com.kh.maison.shop.service.ProductService;
 import com.kh.maison.shop.vo.Product;
 @Controller
@@ -20,7 +22,6 @@ public class ReviewController {
 	@RequestMapping("/shop/insertReview.do")
 	public ModelAndView insertReview(ModelAndView mv,int productNo) {
 		
-		System.out.println("컨트롤러는 오니 ??");
 		Product p=pservice.selectProductOne(productNo);
 		//나중에 오더디테일도 불러와야함
 		//OrderDetail od=;
@@ -31,5 +32,10 @@ public class ReviewController {
 		return mv ;
 	}
 	
+	@RequestMapping("/shop/insertReviewEnd.do")
+	public ModelAndView insertRevuewEnd(ModelAndView mv,@RequestParam Review r) {
+		
+		return mv;
+	}
 	
 }
